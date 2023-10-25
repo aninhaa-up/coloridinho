c68b15934312d0051bd96c707397d26875a9c3c3
aninhaa-up <134522444+aninhaa-up@users.noreply.github.com>
2023-10-25 11:44:33 -0300

    pom

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 alert('hello world')
 function tocaSomPom(){
-    document.querySelector('#som_tecla_pom')
+    document.querySelector('#som_tecla_pom').play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
