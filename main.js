59d210dcd9aa5580e12b7acb318fd5e534e977bf
aninhaa-up <134522444+aninhaa-up@users.noreply.github.com>
2023-10-25 09:08:32 -0300

    Pom

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ alert('hello world')
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom')
 }
-document.querySelector('.tecla_pom').onclick = tocaSompo;
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
