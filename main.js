12f16d4c2b7cac9998f6bf7f0a00c9a5a78ef90a
aninhaa-up <134522444+aninhaa-up@users.noreply.github.com>
2023-10-25 11:40:40 -0300

    clap

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ function tocaSomPom(){
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_cla').onclick = tocaSomClap
